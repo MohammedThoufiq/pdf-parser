54cf5a6296eed732a5a02e52452dca83b3a5621e
Mohammed Thoufiq <thunderfirezzzz1001@gmail.com>
2025-04-08 22:42:36 +0530

    Commiting Changes to Thoufiq Branch

---
+++ Backend/src/main/java/com/example/pdf_parser/PDFAttributes.java
@@ -0,0 +1,28 @@
+package com.example.pdf_parser;
+
+import lombok.Data;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Data
+public class PDFAttributes {
+    private Map<String, String> attributes;
+
+    public PDFAttributes() {
+        this.attributes = new HashMap<>();
+    }
+
+    public void addAttribute(String key, String value) {
+        attributes.put(key, value);
+    }
+
+    public Map<String, String> getAttributes() {
+        return attributes;
+    }
+
+    @Override
+    public String toString() {
+        return "{" + "attributes=" + attributes + '}';
+    }
+}
